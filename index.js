ad56b8e3ec93e5dbf1674f84c8c883ea98d42461
chaturvedi-anil <chaturvedia560@gmail.com>
2023-08-08 23:17:55 +0530

    setup connect-mongodb-session to store session id in mongoDB

--- index.js
+++ index.js
@@ -1,58 +1,70 @@
-//imported express module
-const express=require("express");
-const cookieParser = require('cookie-parser');
-//Define a port number
+// Importing required modules
+const express = require("express");
 const PORT = process.env.PORT || 8000;
-// Create an instance of express application
-const app= express();
+const app = express();
+const cookieParser = require('cookie-parser');
 const expressLayout = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
-// used for session cookie
 const session = require('express-session');
-const passport = require('passport')
-const passportLocal = require('./config/passport-local-strategy');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy'); // Import passport-local strategy configuration
+const MongoDBStore=require('connect-mongodb-session')(session);
 
-app.use(express.urlencoded());
-// cookie setup
+
+// Use express.urlencoded middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
+// Serve static assets from the 'assets' directory
 app.use(express.static('./assets'));
 app.use(expressLayout);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// view engine setup
+// Set the view engine and the views directory
 app.set('view engine', 'ejs');
-// settinge folder for views
 app.set('views', './views');
 
-app.use(session({
-    name: 'habbit-tracker',
-    // TODO change the secret before deployment in production mode
-    secret: 'something',
-    saveUninitialized: false,
-    resave: false,
-    cookie:{
-        maxAge: (1000 * 60 * 100)
+// Configure session and store using connect-mongo
+const store= new MongoDBStore(
+    {
+        // database name its only taking url 
+        uri: 'mongodb://localhost/habbit_tracker_development',
+        collection: 'mySession'
+    },
+    function(err) {
+        console.log(err || 'connect-mongodb setup ok');
     }
-}));
+);
 
+app.use(session(
+    {
+        name: 'habbit-tracker',
+        secret: 'something',   //secret key name for encrypting the cookie
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 1000 * 60 * 100 // Cookie expiration time (in milliseconds)
+        },
+        store: store,
+    }
+));
+
+// Initialize and use Passport authentication middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// set user in locals
-app.use(passport.setAuthenticatedUser);
+// Set authenticated user in locals for views
+app.use(passportLocal.setAuthenticatedUser);
 
-// middleware for routing
+// Middleware for routing
 app.use('/', require('./routes/'));
 
-// Start the server 
-app.listen(PORT, function(err)
-{
-    if(err)
-    {
-        console.log(`Error in runing the server ${err}`);
+// Start the server
+app.listen(PORT, function(err) {
+    if (err) {
+        console.log(`Error in running the server ${err}`);
     }
-    console.log(`server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
